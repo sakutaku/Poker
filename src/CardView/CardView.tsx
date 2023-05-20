86709b0e5f1ef79d98960f8d303b159b90d3f617
Abytova Saikal <saikalabytova1@gmail.com>
2023-05-20 19:43:55 +0600

    Realized first round of the task

---
+++ src/CardView/CardView.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface CardViewProps {
+    rank: string;
+    suit: string;
+}
+
+const CardView: React.FC<CardViewProps> = props => {
+    const cardClass = `card rank-${props.rank.toLowerCase()} ${props.suit}`;
+
+    let suit;
+    
+    if(props.suit === 'diams') {
+        suit = '♦';
+    } else if (props.suit === 'hearts') {
+        suit = '♥';
+    } else if (props.suit === 'clubs') {
+        suit = '♣';
+    } else if (props.suit === 'spades') {
+        suit = '♠';
+    }
+
+    return(
+        <span className={cardClass}>
+            <span className="rank">{props.rank}</span>
+            <span className="suit">{suit}</span>
+        </span>
+    )
+};
+
+export default CardView;
